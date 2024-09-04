227544dd8a240d46aca1c3e0e09800afb5499649
Danylo Litvinchuk <ltvnchkd@gmail.com>
2024-09-04 08:55:32 +0300

    Initial Commit

---
+++ Bookhead/BookheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookheadApp.swift
+//  Bookhead
+//
+//  Created by Danylo Litvinchuk on 04.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookhead/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookhead
+//
+//  Created by Danylo Litvinchuk on 04.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
